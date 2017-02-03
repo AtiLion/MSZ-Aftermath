325edbb4b8beffb904f62e68eaee35b0b3b88cb0
AtiLion <eliteinfinity1337@gmail.com>
2017-02-03 20:01:29 +0100

    Added extra stuff

---
+++ MSZ Aftermath/Debug/DConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using SDG.Unturned;
+
+namespace MSZ_Aftermath.Debug
+{
+    public class DConsole : MonoBehaviour
+    {
+
+
+        public DConsole()
+        {
+
+        }
+    }
+}
